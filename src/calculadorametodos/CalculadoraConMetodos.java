5baf34894fa17adede3894abfb0e9bcd54d53f3e
cristopher01 <cristopher.fs@gmail.com>
2015-10-14 13:34:59 -0300

    prueba02

---
+++ src/calculadorametodos/CalculadoraConMetodos.java
@@ -0,0 +1,52 @@
+package calculadorametodos;
+
+import javax.swing.JOptionPane;
+/*
+        ninguno de los metodos usa parametros ya que es mas facil llamar
+        a los metodos y reiniciar los valores
+        */
+public class CalculadoraConMetodos {
+
+    public static void main(String[] args) {
+/*  
+        No incluye ninguna variable
+        llama a la variable seleccion
+        */    
+    }
+
+    public static void Seleccion() {
+/*  
+        llama al metodo Menu dentro de un switch case para llamar a los
+        diferentes metodos de ejercicio (suma. resta, multiplicacion, etc)
+        */
+    }
+
+    public static void Menu() {
+/*  
+        se usara una variable de tipo int
+        se usa una entrada de un numero entero del cual dependera que ejercicio
+        va a realizar
+        */
+    }
+
+    public static void Entradas() {
+/*
+        se usara una variable de tipo double para ser retornada como el valor
+        del numero que el usuario necesita 
+        */
+    }
+
+    public static void Ejercicios() {
+/*
+        En cada metodo de ejercicio se pediran 2 numeros dependiendo de
+        lo que selecciono el usuario
+        */    
+    }
+
+    public static void Salir() {
+/*
+        El metodo final del recorrido donde se le preguntara el usuario 
+        si realmente quiere salir del programa
+        */
+    }
+}
